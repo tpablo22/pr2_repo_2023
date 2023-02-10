10bd42387e55d37eae755097c68952045aea127d
pablo torres <tpablo851@gmail.com>
2023-02-10 09:54:19 +0100

    practica 1

---
+++ practica1/src/mates/Matematicas.java
@@ -0,0 +1,22 @@
+package mates;
+import java.lang.Math;
+
+public class Matematicas {
+    public static double generarNumeroPiIterativo(double pasos){
+        double aciertos = 0;
+        double areaCuadrado = 4;
+
+        for(int i = 0; i<= pasos;i++){
+            double x = Math.random();
+            double y = Math.random();
+
+            double distancia = ((x*x) + (y*y));
+            if(distancia <= 1){
+                aciertos += 1;
+            }
+        }return areaCuadrado*(aciertos/pasos);
+
+
+    }
+}
+
--- test/src/aplicacion/Principal.java
+++
@@ -1,9 +0,0 @@
-package aplicacion;
-
-//import dominio.*;
-
-public class Principal{
-	public static void main(String args[]){
-		System.out.println("Test");
-	}
-}
