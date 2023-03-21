9e49f2e6d8a253fa0a9ac9e0e312dff4f62aad94
pablo torres <tpablo851@gmail.com>
2023-03-21 14:52:41 +0100

    pp2.4

--- practic2/src/dominio/Tablero.java
+++ practic2/src/dominio/Tablero.java
@@ -6,55 +6,52 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Tablero {
     private static int DIMENSION = 30;
-    private int[][] estadoActual = new int[DIMENSION+2][DIMENSION+2]; //matriz que representa el estado actual.
+    private int[][] estadoActual = new int[DIMENSION+2][DIMENSION+2];
     private int[][] estadoSiguiente= new int[DIMENSION+2][DIMENSION+2];
 
-    // La secuencia de ceros y unos del fichero es guardada
-        // en ‘estadoActual‘ y, utilizando las reglas del juego
-        // de la vida, se insertan los ceros y unos
-        // correspondientes en ‘estadoSiguiente‘.
-        /********************************************************
-         * Genera un estado inicial aleatorio. Para cada celda
-         * genera un número aleatorio en el intervalo [0, 1). Si
-         * el número es menor que 0,5, entonces la celda está
-         * inicialmente viva. En caso contrario, está muerta.
-         *******************************************************/
 
 
-        public void leerEstadoActual() {
-            try {
-                File fichero = new File("matriz");
-                Scanner scanner = new Scanner(fichero);
-                for (int i = 1; i <= DIMENSION; i++) {
-                    String linea = scanner.nextLine();
-                    for (int j = 1; j <= DIMENSION; j++) {
-                        estadoActual[i][j] = Character.getNumericValue(linea.charAt(j - 1));
+    public void leerEstadoActual() {
+        try {
+            File fichero = new File("matriz");
+            Scanner scanner = new Scanner(fichero);
+            for (int i = 1; i <= DIMENSION; i++) {
+                String linea = scanner.nextLine();
+                for (int j = 1; j <= DIMENSION; j++) {
+                    estadoActual[i][j] = Character.getNumericValue(linea.charAt(j-1));
+                }
+            }
+
+            for (int i = 1; i <= DIMENSION; i++) {
+                for (int j = 1; j <= DIMENSION; j++) {
+                    int vecinasVivas = contarVecinasVivas(i, j);
+
+                    if ((estadoActual[i][j] == 1 && vecinasVivas == 2) || vecinasVivas == 3) {
+                        estadoSiguiente[i][j] = 1;
+                    } else {
+                        estadoSiguiente[i][j] = 0;
                     }
                 }
+            }
 
-                for (int i = 1; i <= DIMENSION; i++) {
-                    for (int j = 1; j <= DIMENSION; j++) {
-                        int vecinasVivas = estadoActual[i - 1][j - 1] + estadoActual[i - 1][j] + estadoActual[i - 1][j + 1]
-                                + estadoActual[i][j - 1] + estadoActual[i][j + 1] + estadoActual[i + 1][j - 1] +
-                                estadoActual[i + 1][j] + estadoActual[i + 1][j + 1];
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-                        if (estadoActual[i][j] == 1 && (vecinasVivas == 2 || vecinasVivas == 3)) {
-                            estadoSiguiente[i][j] = 1;
-                        } else if (estadoActual[i][j] == 0 && vecinasVivas == 3) {
-                            estadoSiguiente[i][j] = 1;
-                        } else {
-                            estadoSiguiente[i][j] = 0;
-                        }
-                    }
+    public int contarVecinasVivas(int fila, int columna) {
+        int contador = 0;
+        for (int i = fila - 1; i <= fila + 1; i++) {
+            for (int j = columna - 1; j <= columna + 1; j++) {
+                if (!(i == fila && j == columna)) { // No contamos la celda actual
+                    contador += estadoActual[i][j];
                 }
-
-                scanner.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             }
         }
-
-    public void generarEstadoActualPorMontecarlo() {
+        return contador;
+    }
+            public void generarEstadoActualPorMontecarlo() {
 
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
@@ -99,11 +96,7 @@ public class Tablero {
         StringBuilder cadena = new StringBuilder();
         for (int i = 1; i < DIMENSION+1; i++) {
             for (int j = 1; j < DIMENSION+1; j++) {
-                if (estadoActual[i][j] == 0) {
-                    cadena.append(" ");
-                } else {
-                    cadena.append("x");
-                }
+                cadena.append(estadoActual[i][j] == 0 ? " " : "x");
             }
             cadena.append("\n");
         }
